002bdfbb98696cbd33ac99d72efc5efb4065292d
adamovichhh <egor.adamovichhh@gmail.com>
2023-05-10 11:45:34 +0200

    Add function getting post by id

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createPost, getAll } from '../controllers/posts.js'
+import { createPost, getAll, getById } from '../controllers/posts.js'
 import { checkAuth } from '../utils/checkAuth.js'
 
 const router = new Router()
@@ -13,5 +13,8 @@ router.post('/', checkAuth, createPost)
 // http://localhost:3002/api/posts/
 router.get('/', getAll)
 
+//Get Post By Id
+// http://localhost:3002/api/posts/:id
+router.get('/:id', getById)
 
 export default router
